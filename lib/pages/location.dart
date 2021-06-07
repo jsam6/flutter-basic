deee05c9b38aeb8b644ad92a0219a3b8a5995f59
sam chong <jaysam.chong@minifysolution.com>
2021-06-07 20:57:30 +0800

    ✨ async

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -9,16 +9,31 @@ class _LocationState extends State<Location> {
 
     int counter = 0;
 
+    void getData() async {
+        String number1 = await Future.delayed(Duration(seconds: 3), () {
+            print('1');
+            return '1';
+        });
+
+
+        String number2 = await Future.delayed(Duration(seconds: 2), () {
+            print('2');
+            return '2';
+        });
+
+        print('$number1 - $number2');
+    }
 
     @override
     void initState() {
         super.initState();
-        print('initilize location page');
+        // print('initilize location page');
+        getData();
     }
 
   @override
   Widget build(BuildContext context) {
-        print('build location page');
+        // print('build location page');
     return Scaffold(
         backgroundColor: Colors.cyan[200],
         appBar: AppBar(
