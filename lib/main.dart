9ec07859f139e406f001b35363be1ad4988060e7
sam chong <jaysam.chong@minifysolution.com>
2021-06-07 16:09:43 +0800

    🚧 Statefullwidget and mapping

--- lib/main.dart
+++ lib/main.dart
@@ -6,73 +6,81 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-class Home extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Hack The World',
-                    style: TextStyle(
-                    fontFamily: 'ShadowsIntoLight',)
-                ),
-                centerTitle: true,
-                backgroundColor: Colors.green[600]),
-                body: Row(
-                    children: [
-                        Expanded(
-                            flex: 1,
-                            child: Image.asset('assets/background-image.jpeg'),
-                        ),
-                        Expanded(
-                            flex: 2,
-                            child: Container(
-                                padding: EdgeInsets.all(30),
-                                color: Colors.red,
-                                child: Text('1')
-                            ),
-                        ),
-                        Container(
-                            padding: EdgeInsets.all(30),
-                            color: Colors.blue,
-                            child: Text('2')
-                        ),
-                        Container(
-                            padding: EdgeInsets.all(30),
-                            color: Colors.green,
-                            child: Text('3')
-                        ),
-                    ],
-                ),
-
-                floatingActionButton: FloatingActionButton(
-                    child: Icon(
-                        Icons.add,
-                        color: Colors.black,
-                        size: 30,
-                    ),
-                    onPressed: () {},
-                    backgroundColor: Colors.green[600],
-                ),
-
-
-                bottomNavigationBar: BottomNavigationBar(),
-            );
-    }
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
 }
 
+class _Home extends State<Home> {
+  int counter = 3;
 
+  List<String> quotes = ['test 1', 'test 2', 'test 3'];
 
-
-
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Hack The World',
+              style: TextStyle(
+                fontFamily: 'ShadowsIntoLight',
+              )),
+          centerTitle: true,
+          backgroundColor: Colors.green[600]),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset('assets/background-image.jpeg'),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                    padding: EdgeInsets.all(30),
+                    color: Colors.red,
+                    child: Text('$counter')),
+              ),
+              Container(
+                  padding: EdgeInsets.all(30),
+                  color: Colors.blue,
+                  child: Text('2')),
+              Container(
+                  padding: EdgeInsets.all(30),
+                  color: Colors.green,
+                  child: Text('3')),
+            ],
+          ),
+          Row(
+            children: quotes.map((quote) {
+                return Text(quote);
+            }).toList()
+            
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+          size: 30,
+        ),
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        backgroundColor: Colors.green[600],
+      ),
+      bottomNavigationBar: BottomNavigationBar(),
+    );
+  }
+}
 
 class BottomNavigationBar extends StatelessWidget {
-    const BottomNavigationBar({
-        this.shape = const CircularNotchedRectangle(),
-    });
-
+  const BottomNavigationBar({
+    this.shape = const CircularNotchedRectangle(),
+  });
 
   final NotchedShape? shape;
 
@@ -82,28 +90,27 @@ class BottomNavigationBar extends StatelessWidget {
         color: Colors.green[600],
         shape: shape,
         child: IconTheme(
-            data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                    IconButton(
-                        tooltip: 'Open navigation menu',
-                        icon: const Icon(Icons.menu),
-                        onPressed: () {},
-                    ),
-                    IconButton(
-                        tooltip: 'Favorite',
-                        icon: const Icon(Icons.favorite),
-                        onPressed: () {},
-                    ),
-                    IconButton(
-                        tooltip: 'Search',
-                        icon: const Icon(Icons.search),
-                        onPressed: () {},
-                    ),
-                ],
-                ),
-            )
-    );
+          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                tooltip: 'Open navigation menu',
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              IconButton(
+                tooltip: 'Favorite',
+                icon: const Icon(Icons.favorite),
+                onPressed: () {},
+              ),
+              IconButton(
+                tooltip: 'Search',
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
